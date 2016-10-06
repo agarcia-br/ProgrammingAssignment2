9dd319665c19fd0aefdf5d0df6692226a8c86383
Alex Garcia <agarcia.br@gmail.com>
2016-10-06 13:14:41 -0300

    Programming Assignment week3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## Parameter is a matrix which defaults to empty matrix
+## creates a matrix with four functions, namely
+##     set - sets its data (and invalidates cache)
+##     get - gets its data
+##     setinverse - sets its inverse into cache
+##     getinverse - gets its inverse from cache 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrixData = matrix()) {
+  matrixInverse <- NULL
+  set <- function(y) {
+    matrixData <<- y
+    matrixInverse <<- NULL
+  }
+  get <- function() matrixData
+  setinverse <- function(inverse) matrixInverse <<- inverse
+  getinverse <- function() matrixInverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## First parameter is a matrix created by makeCacheMatrix
+## May accept other ... parameters which will be passed down to solve() function 
+## Caches the inverse of 'x' and also returns it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+##.........testCase...........
+##> A <- matrix(c(2, 4, 3, 1, 5, 7, 2, 9, 1), nrow=3, ncol=3) 
+##> makeCacheMatrix(A)
+##> A$getinverse()
+##NULL
+##> cacheSolve(A)
+##[,1]        [,2]        [,3]
+##[1,]  0.8656716 -0.19402985  0.01492537
+##[2,] -0.3432836  0.05970149  0.14925373
+##[3,] -0.1940299  0.16417910 -0.08955224
+##> A$getinverse()
+##[,1]        [,2]        [,3]
+##[1,]  0.8656716 -0.19402985  0.01492537
+##[2,] -0.3432836  0.05970149  0.14925373
+##[3,] -0.1940299  0.16417910 -0.08955224
